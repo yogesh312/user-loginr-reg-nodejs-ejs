bd84ca41a86a5a6f742612c67936b6c742092f38
yogesh <etpr.yogesh@gmail.com>
2021-08-31 23:05:35 +0530

    establishing db and gettting routes in server

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose Connection ERROR: " + err.message);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected!");
+});
+
+//Bring in the models
+require("./models/User");
+
+
+const app = require("./app");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
+
+const io = require("socket.io")(server);
+const jwt = require("jwt-then");
+
+
+const User = mongoose.model("User");
+
