ac350a1f9738297091dd78ce749c6924ea3178e7
yogesh <etpr.yogesh@gmail.com>
2021-09-06 23:10:36 +0530

    app.js and dependencies updated

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const express = require("express");
 
 const app = express();
 
+//include express layouts
+const expressLayouts = require('express-ejs-layouts');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Setup Cross Origin
 app.use(require("cors")());
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//set assets path
+app.use(express.static('./assets'));
+
+//use express layouts
+app.use(expressLayouts);
+
+//extract styles and scripts from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
 
 //Bring in the routes
 app.use("/user", require("./routes/user"));
