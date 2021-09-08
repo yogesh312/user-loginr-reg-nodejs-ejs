27bdf1f24936413753de7b0cdaa93bdb119d6a92
yogesh <etpr.yogesh@gmail.com>
2021-09-08 23:40:55 +0530

    routes modification

--- routes/contact.js
+++ routes/contact.js
@@ -7,6 +7,8 @@ const auth = require("../middlewares/auth");
 
 router.get("/all", auth, catchErrors(contactController.getAllContact));
 router.post("/create", auth, catchErrors(contactController.createContact));
-router.put("/edit/:_id", auth, catchErrors(contactController.editContact));
+router.get("/create", auth, catchErrors(contactController.createContact1));
+router.put("/edit/:name", auth, catchErrors(contactController.editContact));
+router.get("/edit/:name", auth, catchErrors(contactController.editContact1));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ router.post("/login", catchErrors(userController.login));
 router.get("/login", catchErrors(userController.logins));
 router.post("/register", catchErrors(userController.register));
 router.get("/register", catchErrors(userController.registers));
+router.get("/", catchErrors(userController.home));
 
 module.exports = router;
